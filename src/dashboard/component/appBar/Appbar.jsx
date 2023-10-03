a2c366a6de03c5e6b6289712373b2d8a1371168c
Upendra Sahu <upendrasahu1199@gmail.com>
2023-10-03 13:49:26 +0530

    [Upendra] Add:Added Logout Feature Which Delete The Token In Local Storage

--- src/dashboard/component/appBar/Appbar.jsx
+++ src/dashboard/component/appBar/Appbar.jsx
@@ -16,11 +16,19 @@ import MailIcon from '@mui/icons-material/Mail';
 import SettingsIcon from '@mui/icons-material/Settings' // setting icon
 import SplitscreenSharpIcon from '@mui/icons-material/SplitscreenSharp'; //List view Icon
 import RefreshSharpIcon from '@mui/icons-material/RefreshSharp'; //Refresh screen Icon
+import keepLogo from "../../../asset/keepLogo.png"
 // import logo from '../asset/keepLogo'
 // import NotificationsIcon from '@mui/icons-material/Notifications';
 // import MoreIcon from '@mui/icons-material/MoreVert';
 import "./Appbaar.css"
 
+
+function handleLogout() {
+  // Remove the token from local storage
+  localStorage.removeItem('Token');
+}
+
+//Define a styled Search component
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
   borderRadius: theme.shape.borderRadius,
@@ -37,6 +45,8 @@ const Search = styled('div')(({ theme }) => ({
   },
 }));
 
+
+//Define a styled SearchIconWrapper component
 const SearchIconWrapper = styled('div')(({ theme }) => ({
   padding: theme.spacing(0, 2),
   height: '100%',
@@ -47,6 +57,8 @@ const SearchIconWrapper = styled('div')(({ theme }) => ({
   justifyContent: 'center',
 }));
 
+
+//Define a styled InputBase component
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: 'inherit',
   '& .MuiInputBase-input': {
@@ -61,30 +73,39 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-export default function Appbar() {
+
+//Define the Appbar component
+export default function Appbar(props) {
+  //State for men anchors
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
+  //Chec if meny isopen
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
+  //Handle opening user account menu
   const handleProfileMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
+  //Handle closing mobile menu
   const handleMobileMenuClose = () => {
     setMobileMoreAnchorEl(null);
   };
 
+  //Handle closing menus
   const handleMenuClose = () => {
     setAnchorEl(null);
     handleMobileMenuClose();
   };
-
+  
+  //Handle opening mobile menu
   const handleMobileMenuOpen = (event) => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
+  //Define menu IDs
   const menuId = 'primary-search-account-menu';
   const renderMenu = (
     <Menu
@@ -101,11 +122,12 @@ export default function Appbar() {
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Logout</MenuItem> {/*4 step logout button */}
+    >{/* Logout button */}
+      <MenuItem onClick={handleLogout}>Logout</MenuItem> {/*4 step logout button */}
     </Menu>
   );
 
+  //Define mobile menu Id
   const mobileMenuId = 'primary-search-account-menu-mobile';
   const renderMobileMenu = (
     <Menu
@@ -123,14 +145,6 @@ export default function Appbar() {
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
-      <MenuItem>
-        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
       <MenuItem>
         <IconButton
           size="large"
@@ -156,19 +170,34 @@ export default function Appbar() {
     </Menu>
   );
 
+  // Function to handle opening the side drawer
+  const handleDrawerOpen = () => {
+    props.handleDrawerOpen();
+  }
+
   return (
+    <div>
     <Box sx={{ flexGrow: 1}}>
       <AppBar position="static" sx={{ backgroundColor: 'white', color:"gray" /* 1 step give color*/ }} >
         <Toolbar>
+          {/* Menu icon button */}
           <IconButton
-            size="large"
-            edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{ mr: 2 }}
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+            }}
           >
             <MenuIcon />
           </IconButton>
+          {/* Application logo */}
+          <img src={keepLogo} alt="" style={{
+            width:'70px'
+          }}/>
+
+          {/* Application title */}
           <Typography
             variant="h6"
             noWrap
@@ -177,11 +206,14 @@ export default function Appbar() {
         }}
           >
             FundooNotes   
-          </Typography>     
+          </Typography>  
+          {/* Search bar*/}   
           <Search className='Serach-Container' >
+            {/* Search icon */}
             <SearchIconWrapper>
               <SearchIcon />
             </SearchIconWrapper>
+            {/* Input field for searching */}
             <StyledInputBase
               placeholder="Search…"
               inputProps={{ 'aria-label': 'search' }}
@@ -189,7 +221,10 @@ export default function Appbar() {
               className='Input'
             />
           </Search>
+          {/* Spacer*/}
           <Box sx={{ flexGrow: 1 }} />
+
+          {/* Icon buttons for larger screens */}
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
 
            <IconButton aria-label='Refresh Screen'> {/* 5 Step Refresh Screen Button*/}
@@ -204,18 +239,15 @@ export default function Appbar() {
             <SettingsIcon />
             </IconButton>
 
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
+            {/* <IconButton
               size="large"
               aria-label="show 17 new notifications"
               color="inherit"
             >
               
-            </IconButton>
+            </IconButton> */}
+
+            {/* User account button*/}
             <IconButton
               size="large"
               edge="end"
@@ -228,6 +260,8 @@ export default function Appbar() {
               <AccountCircle />
             </IconButton>
           </Box>
+
+          {/* Icon buttons for smaller screens */}
           <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
@@ -245,5 +279,6 @@ export default function Appbar() {
       {renderMobileMenu}
       {renderMenu}
     </Box>
+    </div>
   );
 }
