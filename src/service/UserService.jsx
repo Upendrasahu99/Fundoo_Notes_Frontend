3cff70e4f1d3078232c78e4d805fb1e9c32a76d5
Upendra Sahu <upendrasahu1199@gmail.com>
2023-11-03 07:34:51 +0530

    [Upendra] Update Service

--- src/service/UserService.jsx
+++ src/service/UserService.jsx
@@ -1,11 +1,14 @@
 //import axios from "axios"
 const axios = require("axios");
 
-export const signup = async(obj) => {
-        let response = await axios.post('https://fundoobackend.azurewebsites.net/api/User/Register', obj);
-        return response;
-    }
+export const signup = async (obj) => {
+  const response = await axios.post(
+    "https://fundoobackend.azurewebsites.net/api/User/Register",
+    obj
+  );
 
+  return response.data;
+};
 export const login = async(obj) => {
     let response = await axios.post('https://localhost:44304/api/User/Login', obj);
     return response
